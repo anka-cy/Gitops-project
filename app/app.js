33f25e85a09a5ef9b5e3a4d1639c71118a02f253
anka-cy <aymanbennassadia00@gmail.com>
2026-06-08 00:46:19 +0100

    landing page

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/health", (req, res) => {
   res.json({
     message: "GitOps pipeline is working!!",
     status: "success"
   });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(3000, () => {
