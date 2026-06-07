115798a4d470aeed0cda7d3076d2220483df6cfb
anka-cy <aymanbennassadia00@gmail.com>
2026-06-07 21:33:06 +0100

    Update success message in root route response

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "GitOps CI/CD Pipeline Running Successfully!",
+    message: "GitOps CI/CD Pipeline Running!",
     status: "success"
   });
 });
