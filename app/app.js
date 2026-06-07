16e13aeca54f60b84fdffd476b5880aaff28ff1e
anka-cy <aymanbennassadia00@gmail.com>
2026-06-07 18:52:27 +0100

    update

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "GitOps CI/CD Pipeline Running 🚀",
+    status: "success"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
