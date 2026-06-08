2a9c4721cbc2ee103a0a8039bb53f867af7e250e
anka-cy <aymanbennassadia00@gmail.com>
2026-06-08 01:09:41 +0100

    feat: instrument app with prometheus metrics and add .gitignore

--- app/app.js
+++ app/app.js
@@ -2,8 +2,44 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const client = require('prom-client');
+
+// Create a Registry which registers the metrics
+const register = new client.Registry();
+
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'hello-gitops'
+});
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
+
+// Create a custom histogram metric for HTTP requests
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in microseconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+register.registerMetric(httpRequestDurationMicroseconds);
+
+// Middleware to measure request duration
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route ? req.route.path : req.path, code: res.statusCode });
+  });
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// Expose metrics endpoint for Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get("/api/health", (req, res) => {
   res.json({
